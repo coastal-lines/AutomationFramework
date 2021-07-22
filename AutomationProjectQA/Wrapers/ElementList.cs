334a80247581900dc2827c7b1f6eab452dcb8c86
paul <piankin250@gmail.com>
2021-07-22 23:11:30 +0300

    QA automation

---
+++ AutomationProjectQA/Wrapers/ElementList.cs
@@ -0,0 +1,30 @@
+﻿using AutomationProjectQA.FrameworkCore.Driver;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+
+namespace AutomationProjectQA.Wrapers
+{
+    public class ElementList
+    {
+        private IList<Element> _element;
+        private IWebDriver _driver;
+        private By _by;
+        private WebDriverWait _wait;
+
+        private const int DefaultWaitTimeoutSeconds = 60;
+
+        public ElementList(By by)
+        {
+            _by = by;
+            _driver = DriverClass.GetDriver();
+            _wait = new WebDriverWait(DriverClass.GetDriver(), TimeSpan.FromSeconds(DefaultWaitTimeoutSeconds));
+        }
+
+        private void InstantiateElement()
+        {
+            _element = _driver.FindElements(_by);
+        }
+    }
+}
