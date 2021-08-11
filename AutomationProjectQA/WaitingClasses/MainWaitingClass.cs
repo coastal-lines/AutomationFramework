d7ede58ab3f470e600f3550d4a9a7e053bf6948d
paul <piankin250@gmail.com>
2021-08-11 23:49:36 +0300

    Merge branch 'master' of https://github.com/coastal-lines/AutomationFrameworkQA automation

--- AutomationProjectQA/WaitingClasses/MainWaitingClass.cs
+++ AutomationProjectQA/WaitingClasses/MainWaitingClass.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace AutomationProjectQA.WaitingClasses
 {
-    public class MainWaitingClass
+    public static class MainWaitingClass
     {
+        public static void WaitElement(IWebDriver driver, By by)
+        {
+            WebDriverWait w = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            w.Until(ExpectedConditions.ElementExists(by));
+        }
     }
 }
