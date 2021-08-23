dc34498c9e953d603beeb1bcd40e1ea2818a9a53
paul <piankin250@gmail.com>
2021-08-23 23:12:29 +0300

    QA automation

---
+++ AutomationProjectQA/HelpersMethods/ElementActions.cs
@@ -0,0 +1,19 @@
+﻿using AutomationProjectQA.FrameworkCore.Driver;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace AutomationProjectQA.HelpersMethods
+{
+    public class ElementActions
+    {
+        public void ClickBySelenium(IWebElement element)
+        {
+            element.Click();
+        }
+
+        public void DoubleClickBySelenium(IWebElement element)
+        {
+            new Actions(DriverClass.GetDriver()).DoubleClick(element).Build().Perform();
+        }
+    }
+}
