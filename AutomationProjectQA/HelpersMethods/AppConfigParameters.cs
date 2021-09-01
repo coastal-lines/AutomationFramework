44b227aabb7138fff21f472e7efe56338a3623cd
paul <piankin250@gmail.com>
2021-09-01 21:45:44 +0300

    QA automation

---
+++ AutomationProjectQA/HelpersMethods/AppConfigParameters.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace AutomationProjectQA.HelpersMethods
+{
+    public static class AppConfigParameters
+    {
+        public static string GetDefaultBrowser()
+        {
+            return ConfigurationManager.AppSettings["defaultBrowser"].ToString();
+        }
+    }
+}
