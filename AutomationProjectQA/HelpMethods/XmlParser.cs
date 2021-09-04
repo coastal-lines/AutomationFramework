7bd2542716011201edb920c359d1b0567bee821a
paul <piankin250@gmail.com>
2021-09-04 14:03:05 +0300

    QA automation

---
+++ AutomationProjectQA/HelpMethods/XmlParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AutomationProjectQA.HelpMethods
+{
+    public class XmlParser
+    {
+        public XmlElement LoadXmlFromFileAndGetRoot(string filePath)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filePath);
+            XmlElement xmlRoot = xmlDoc.DocumentElement;
+
+            return xmlRoot;
+        }
+    }
+}
