949508a249ec4ef1185323715ade02502194ddb8
paul <piankin250@gmail.com>
2021-10-30 23:45:19 +0300

    QA automation

---
+++ AutomationProjectQA/HelpMethods/Office/CSV.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationProjectQA.HelpMethods.Office
+{
+    public class CSV
+    {
+        private List<string> GetAllCellsFromCsvFile(string filePath)
+        {
+            var cellsList = new List<string>();
+            using (var reader = new StreamReader(filePath))
+            {
+                string[] rows;
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    rows = line.Split(';');
+                    foreach (var row in rows)
+                    {
+                        var cells = row.Split(',');
+                        foreach (var cell in cells)
+                        {
+                            cellsList.Add(cell.Replace("\"", "").Replace("\\", ""));
+                        }
+                    }
+                }
+            }
+
+            return cellsList;
+        }
+    }
+}
